ded91f4ebb85de17f57d9a41fafbdffa0b90fd8e
rafael millan <millanr231@gmail.com>
2025-10-27 04:00:31 +0000

    Fix: Build Firebase config from env vars

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,7 +4,6 @@
 import { getApp, getApps, initializeApp, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
-import { firebaseConfig } from './config';
 
 import { useCollection } from './firestore/use-collection';
 import { useDoc } from './firestore/use-doc';
@@ -16,6 +15,18 @@ import {
   useFirestore,
 } from './provider';
 import { FirebaseClientProvider } from './client-provider';
+
+// Construct Firebase config from environment variables
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
+
 
 // This function ensures Firebase is initialized only once.
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
